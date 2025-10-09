4db995abdd2f25e8a1221e5dd8a0e4b5d481d5aa
Andrew Burdyug <buran83@gmail.com>
2025-10-09 21:37:47 +0300

    Refactoring

--- main.go
+++ main.go
@@ -4,30 +4,11 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
-	"time"
 
-	"github.com/charmbracelet/lipgloss"
 	_ "github.com/joho/godotenv/autoload"
 
 	"gitlab.com/1buran/hhbot/internal/infrastructure/apiclient"
 	"gitlab.com/1buran/hhbot/internal/infrastructure/apiclient/auth"
-)
-
-var (
-	vacancyCard   = lipgloss.NewStyle().PaddingLeft(4)
-	redflagStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("#FF6863"))
-	appliedStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("#80EF80"))
-	invitedStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("#00FF00"))
-	questionStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#00F0FF"))
-	favoriteStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#FF7518"))
-
-	blacklistedStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFEF00"))
-	greenlightStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("121"))
-
-	salaryStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFD700"))
-	titleStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("#dddddd")).Bold(true)
-	companyStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("212")).Italic(true)
 )
 
 func main() {
@@ -62,12 +43,6 @@ func main() {
 		return
 	}
 
-	relations, ok := dict.Get("vacancy_relation")
-	if !ok {
-		fmt.Printf("Warning: vacancy_relation not found! dict: %+v\n", dict)
-	}
-	fmt.Printf("Виды связи: %+v\n", relations)
-
 	fmt.Println("\n=== Searching for vacancies ===")
 	vacancies, err := client.SearchVacancies("Golang developer", 0)
 	if err != nil {
@@ -79,55 +54,6 @@ func main() {
 		if v.Archived {
 			continue
 		}
-		var rel []string
-		for _, k := range v.Relations {
-			v := relations.Get(k)
-			if v != "" {
-				switch k {
-				case "favorite":
-					v = favoriteStyle.Render(v)
-				case "got_response":
-					v = appliedStyle.Render(v)
-				case "got_rejection":
-					v = redflagStyle.Render(v)
-				case "got_invitation":
-					v = invitedStyle.Render(v)
-				case "blacklisted":
-					v = blacklistedStyle.Render(v)
-				case "got_question":
-					v = questionStyle.Render(v)
-				}
-				rel = append(rel, v)
-			}
-		}
-
-		var experience string
-		switch v.Experience.Id {
-		case "moreThan6":
-			experience = redflagStyle.Render(v.Experience.Name)
-		case "between1And3", "noExperience":
-			experience = greenlightStyle.Render(v.Experience.Name)
-		default:
-			experience = v.Experience.Name
-		}
-
-		fmt.Printf("%d. %s / %s\n", i+1, titleStyle.Render(v.Name),
-			salaryStyle.Render(v.Salary_range.String()))
-		fmt.Println(
-			vacancyCard.Render(
-				fmt.Sprintf(
-					"Компания: %s\nОпыт: %s\nСвязь: %s\nФормат работы: %s\n"+
-						"Откликнулось: %d / %d\nОпубликовано: %s\nСоздано: %s\n%s\n\n",
-					companyStyle.Render(v.Employer.Name),
-					experience,
-					strings.Join(rel, ", "),
-					v.Work_format.String(),
-					v.Counters.Responses, v.Counters.Total_responses,
-					v.Published_at.Format(time.DateOnly),
-					v.Created_at.Format(time.DateOnly),
-					v.Alternate_url,
-				),
-			),
-		)
+		renderVacancy(i, v, dict)
 	}
 }
---
+++ render.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/charmbracelet/lipgloss"
+	_ "github.com/joho/godotenv/autoload"
+
+	"gitlab.com/1buran/hhbot/internal/infrastructure/apiclient/dto"
+)
+
+var (
+	vacancyCard   = lipgloss.NewStyle().PaddingLeft(4)
+	redflagStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("#FF6863"))
+	appliedStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("#80EF80"))
+	invitedStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("#00FF00"))
+	questionStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#00F0FF"))
+	favoriteStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#FF7518"))
+
+	blacklistedStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFEF00"))
+	greenlightStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("121"))
+
+	salaryStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFD700"))
+	titleStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("#dddddd")).Bold(true)
+	companyStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("212")).Italic(true)
+)
+
+func renderExperience(exp dto.DictionaryItem) (experience string) {
+	switch exp.Id {
+	case "moreThan6":
+		experience = redflagStyle.Render(exp.Name)
+	case "between1And3", "noExperience":
+		experience = greenlightStyle.Render(exp.Name)
+	default:
+		experience = exp.Name
+	}
+	return
+}
+
+func renderRelations(relations []string, dict dto.Dictionary) string {
+	relationsDict, ok := dict.Get("vacancy_relation")
+	if !ok {
+		fmt.Printf("Warning: vacancy_relation not found! dict: %+v\n", dict)
+	}
+
+	var rel []string
+	for _, k := range relations {
+		v := relationsDict.Get(k)
+		if v != "" {
+			switch k {
+			case "favorite":
+				v = favoriteStyle.Render(v)
+			case "got_response":
+				v = appliedStyle.Render(v)
+			case "got_rejection":
+				v = redflagStyle.Render(v)
+			case "got_invitation":
+				v = invitedStyle.Render(v)
+			case "blacklisted":
+				v = blacklistedStyle.Render(v)
+			case "got_question":
+				v = questionStyle.Render(v)
+			}
+			rel = append(rel, v)
+		}
+	}
+	return strings.Join(rel, ", ")
+}
+
+func renderVacancy(i int, v dto.Vacancy, dict dto.Dictionary) {
+	renderedRelations := renderRelations(v.Relations, dict)
+	experience := renderExperience(v.Experience)
+
+	fmt.Printf("%d. %s / %s\n", i+1, titleStyle.Render(v.Name),
+		salaryStyle.Render(v.Salary_range.String()))
+	fmt.Println(
+		vacancyCard.Render(
+			fmt.Sprintf(
+				"Компания: %s\nОпыт: %s\nСвязь: %s\nФормат работы: %s\n"+
+					"Откликнулось: %d / %d\nОпубликовано: %s\nСоздано: %s\n%s\n\n",
+				companyStyle.Render(v.Employer.Name),
+				experience,
+				renderedRelations,
+				v.Work_format.String(),
+				v.Counters.Responses, v.Counters.Total_responses,
+				v.Published_at.Format(time.DateOnly),
+				v.Created_at.Format(time.DateOnly),
+				v.Alternate_url,
+			),
+		),
+	)
+}
