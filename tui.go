8acd0ad66cbab4eb9703165dbaa8433f636767bc
Andrew Burdyug <buran83@gmail.com>
2025-10-10 18:29:34 +0300

    Refactoring

--- tui.go
+++ tui.go
@@ -14,6 +14,13 @@ import (
 	"strings"
 )
 
+const (
+	ListVacancies = iota // views, screens with different content type
+	VacancyContent
+	ApplyToVacancy
+	ErrorMessage
+)
+
 var (
 	glamourRenderer, _ = glamour.NewTermRenderer(
 		glamour.WithStylePath("dracula"),
@@ -41,44 +48,53 @@ func (m model) Init() tea.Cmd {
 
 func (m model) View() string {
 	switch m.view {
-	case 1:
+	case ErrorMessage:
+		return redflagStyle.Render(m.content...)
+	case VacancyContent:
 		lines := len(m.content)
 		if lines <= m.h { // all content fits the one screen without scroll
 			return strings.Join(m.content, "\n")
 		}
 		return strings.Join(m.content[m.scroll:m.scroll+m.h-1], "\n")
+	case ApplyToVacancy:
+		return DialogYesNo("Откликнуться на вакансию?")
 	}
+
+	// default: show list of vacancies
 	return renderVacancy(m.cursor, m.vacancies[m.cursor], m.hhdict)
 }
 
-type viewVacancy struct{}
-
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-	case viewVacancy, error:
-		m.view = 1
-		return m, nil
 	case tea.WindowSizeMsg:
 		m.w, m.h = msg.Width, msg.Height
-		return m, nil
 	case tea.KeyMsg:
 		switch msg.String() {
 		// These keys should exit the program.
 		case "ctrl+c", "q":
-			if m.view == 1 { // quit from preview of vacancy
-				m.view = 0
+			if m.view == VacancyContent { // quit from preview of vacancy
+				m.view = ListVacancies
 				return m, nil
 			}
 			return m, tea.Quit
-
+		case "a":
+			m.view = ApplyToVacancy
+		case "n":
+			if m.view == ApplyToVacancy {
+				m.view = VacancyContent
+			}
+		case "y":
+			if m.view == ApplyToVacancy {
+				// todo add apply to vacancy
+			}
 		// The "up" and "k" keys move the cursor up
 		case "up", "k":
 			switch m.view {
-			case 0:
+			case ListVacancies:
 				if m.cursor > 0 {
 					m.cursor--
 				}
-			case 1:
+			case VacancyContent:
 				if m.scroll > 0 {
 					m.scroll--
 				}
@@ -87,11 +103,11 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		// The "down" and "j" keys move the cursor down
 		case "down", "j":
 			switch m.view {
-			case 0:
+			case ListVacancies:
 				if m.cursor < len(m.vacancies)-1 {
 					m.cursor++
 				}
-			case 1:
+			case VacancyContent:
 				if len(m.content)-m.scroll > m.h {
 					m.scroll++
 				}
@@ -103,11 +119,11 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			vacancyID := m.vacancies[m.cursor].Id
 			vac, err := m.hhclient.GetVacancy(vacancyID)
 			if err != nil {
-				m.content = []string{redflagStyle.Render(err.Error())}
-				return m, func() tea.Msg {
-					return fmt.Errorf("GetVacancy failure: %w", err)
-				}
+				m.view = ErrorMessage
+				m.content = []string{fmt.Errorf("GetVacancy failure: %w", err).Error()}
+				return m, nil
 			}
+
 			var skills string
 			if vac.Key_skills.Length() > 0 {
 				skills = fmt.Sprintf("**Skills**: %s\n", vac.Key_skills.String())
@@ -115,23 +131,20 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 			md, err := html2md.ConvertString(vac.Description)
 			if err != nil {
-				m.content = []string{redflagStyle.Render(err.Error())}
-				return m, func() tea.Msg {
-					return fmt.Errorf("html2md.ConvertString failure: %w", err)
-				}
+				m.view = ErrorMessage
+				m.content = []string{fmt.Errorf("html2md.ConvertString failure: %w", err).Error()}
+				return m, nil
 			}
 
 			renderedContent, err := glamourRenderer.Render(skills + md)
 			if err != nil {
-				m.content = []string{redflagStyle.Render(err.Error())}
-				return m, func() tea.Msg {
-					return fmt.Errorf("glamour.Render failure: %w", err)
-				}
+				m.view = ErrorMessage
+				m.content = []string{fmt.Errorf("glamour.Render failure: %w", err).Error()}
+				return m, nil
 			}
+
+			m.view = VacancyContent
 			m.content = strings.Split(renderedContent, "\n")
-			return m, func() tea.Msg {
-				return viewVacancy{}
-			}
 		}
 	}
 
